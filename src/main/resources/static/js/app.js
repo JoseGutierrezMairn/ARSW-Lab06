fd845793517264adfe0dad9ec0556c4882ae086f
Jose Gutierrez Marin <andres.gutierrez-m@mail.escuelaing.edu.co>
2021-03-09 10:21:37 -0500

    Canvas done

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,6 @@
 var mock = apimock;
 var app = (function(){
-	
+
 	var mapeo = function(lista){
 		return mapped = lista.map(function(blueprint){
 			return {name:blueprint.name, pts:blueprint.points.length};
@@ -8,6 +8,7 @@ var app = (function(){
 		})
 	}
 	var table = function(bps){
+		
 		if(bps === undefined){
 			alert("Author does not exist");
 		}else{
@@ -18,8 +19,20 @@ var app = (function(){
             })
 		
 		$("#totalPoints").text("Total points: " + r);
+		var vpname = "Blueprint name";
+		var nop = "Number of points";
+		var ac  = "Action";
+		$("#tabla tbody").empty();
+		var temp = null;
+		var markup = null;
 		mapped.map(function(bp){
-			var markup = "<tr><th>"+bp.name+"</td><th>"+bp.pts;
+			
+			if(temp == null){
+				markup = '<tr><th>'+vpname+'</th><th>'+nop+'</th><th>'+ac+'</th></tr><tr><th>'+bp.name+'</th><th>'+bp.pts+'</th><th type="button" onclick="app.draw(\''+bp.name+'\')">Open</th></tr>';
+				temp = "a";
+			}else{
+				markup = '<tr><th>'+bp.name+'</th><th>'+bp.pts+'</th><th type="button" onclick="app.draw(\''+bp.name+'\')">Open</th></tr>';
+			}
 			$("table > tbody").append(markup);
 		});
 		}
@@ -28,9 +41,33 @@ var app = (function(){
 	var queue = function(author){
 		mock.getBlueprintsByAuthor(author, table);
 	}
+	
+	
+	var drawCanvas = function(blueprint){
+			var c = document.getElementById("myCanvas");
+			var ctx = c.getContext("2d");
+			var dx = null;
+			var dy = null;
+			ctx.clearRect(0, 0, 500, 300);
+			ctx.beginPath();
+			blueprint.points.map(function(point){
+				if(dx == null){
+					dx = point.x;
+					dy = point.y;
+					ctx.moveTo(dx, dy);
+				}else{
+					ctx.lineTo(point.x, point.y);
+				}
+			});
+			ctx.stroke();
+	}
+	
 	return{
-		queue : queue
-		
+		queue : queue,
+		draw : function(name){
+			author = document.getElementById("authorInput").value;
+			mock.getBlueprintsByNameAndAuthor(name,author,drawCanvas);
+		}
 	};
 	
 })();
